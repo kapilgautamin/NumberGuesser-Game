de8142d2c00df0b37f1b96715ca7b92ff5c47ecf
Kapil Gautam <kapil.gautam@utdallas.edu>
2020-04-03 20:37:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,24 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => Math.floor(Math.random()*9);
+// console.log(generateTarget());
 
+const compareGuesses = (userGuess, compGuess, target) => {
+  if(userGuess < 0 || userGuess > 9)
+    alert('Enter a number between 0 and 9');
+  let userDiff = Math.abs(userGuess - target);
+  let compDiff = Math.abs(compGuess - target);
+  if(userDiff <= compDiff)
+    return true;
+  return false;
+};
+
+const updateScore = winner => {
+  if(winner === 'human')
+    humanScore++;
+  else
+    computerScore++;
+}
+
+const advanceRound = () => currentRoundNumber++;
